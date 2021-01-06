a216390cfa7add54789e801f664bd3af238f9796
Shanta Mohanty <shantamohanty@yahoo.in>
2021-01-06 10:38:56 +0530

    All done

--- address_book/urls.py
+++ address_book/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import HomePageView, AddressPageView
+from .views import HomePageView, AddressPageView, add_address, edit_address, delete_address
 
 urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
-    path('add_address/', AddressPageView.as_view(), name='add_address'),
+    path('add_address/', add_address, name='add_address'),
+    path('edit/<list_id>', edit_address, name="edit"),
+    path('delete/<list_id>', delete_address, name="delete"),
 ]
--- address_book/views.py
+++ address_book/views.py
@@ -1,10 +1,52 @@
-from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.shortcuts import render, redirect
+from django.views.generic import TemplateView, ListView
+from .models import Address
+from .forms import AddressForm
+from django.contrib import messages
 
-class HomePageView(TemplateView):
+class HomePageView(ListView):
+    model = Address
     template_name='home.html'
+    context_object_name = 'addresses'
 
 class AddressPageView(TemplateView):
     template_name='add_address.html'  
 
+def add_address(request):
+    if request.method == 'POST':
+        form = AddressForm(request.POST or None)
+        if form.is_valid():
+            form.save()
+            messages.success(request, ('Address Has Been Added'))
+            return redirect('home')
+        else:
+            messages.success(request, ('Seems like there was an error'))
+            return render(request,'add_address.html')
+    else:
+        return render(request, 'add_address.html', {})
 # Create your views here.
+
+def edit_address(request, list_id):
+    if request.method == 'POST':
+        current_address = Address.objects.get(pk=list_id)
+        form = AddressForm(request.POST or None, instance=current_address)
+        if form.is_valid():
+            form.save()
+            messages.success(request, ('Address Has Been Edited'))
+            return redirect('home')
+        else:
+            messages.success(request, ('Seems like there was an error'))
+            return render(request,'edit.html')
+    else:
+        get_address = Address.objects.get(pk=list_id)
+        return render(request, 'edit.html', {'get_address': get_address})
+
+def delete_address(request, list_id):
+    if request.method == 'POST':
+        current_address = Address.objects.get(pk=list_id)
+        current_address.delete()
+        messages.success(request, ('Address Has Been Deleted'))
+        return redirect('home')
+    else:
+        messages.success(request, ('Nothing to see here'))
+        return redirect('home')
